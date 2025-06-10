fc1af6043fb40d518fc4c6d9b0e1f1dafb91ae43
LuisLozano23 <desarrolladorlozano23@gmail.com>
2025-06-09 22:10:12 -0500

    Limpiar archivos bloqueados en .vs e ignorarlos correctamente

--- School.Api/obj/Debug/net8.0/School.Api.AssemblyInfo.cs
+++ School.Api/obj/Debug/net8.0/School.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a976dbc697339e00c63e903469317118faa3ad2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("School.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
